d6a5655ca11ee3326c5e4c70e75f7d0cfa769744
Tom Parker <palfrey@tevp.net>
2017-08-13 15:26:13 +0100

    Refactor core to potentially eventually do non-b98 tests

--- test/clostridium/core_test.clj
+++ test/clostridium/core_test.clj
@@ -4,17 +4,23 @@
    [clostridium.core :refer [runBefunge]]
    [clojure.pprint :refer [pprint]]))
 
+(defn without-extension [path]
+  (clojure.string/join "." (butlast (clojure.string/split path #"\."))))
+
+(defn extension [path]
+  (last (clojure.string/split path #"\.")))
+
 (defmacro runPyfungeTests []
   (cons 'do
         (doall
-         (let [folder (java.io.File. "test/pyfunge/befunge98")]
-           (for [testFile (filter #(.endsWith (.getName %) ".b98") (.listFiles folder))
-                 :let [testPath (.getAbsolutePath testFile)
-                       shortname (str (.getName folder) "-" (.replace (.getName testFile) ".b98" ""))]]
-             `(deftest ~(symbol shortname)
-                (let [expected# (slurp ~(.replace testPath ".b98" ".expected"))
-                      trimExpected# (subs expected# 0 (- (count expected#) 1))
-                      result# (with-out-str (runBefunge ~testPath))]
-                  (is (= trimExpected# result#)))))))))
+         (for [folder (filter #(.contains ["befunge98"] (.getName %)) (.listFiles (java.io.File. "test/pyfunge")))
+               testFile (filter #(.contains ["b98" "bf" "t98" "u98"] (extension (.getName %))) (.listFiles folder))
+               :let [testPath (.getAbsolutePath testFile)
+                     shortname (str (.getName folder) "-" (without-extension (.getName testFile)))]]
+           `(deftest ~(symbol shortname)
+              (let [expected# (slurp ~(str (without-extension testPath) ".expected"))
+                    trimExpected# (subs expected# 0 (- (count expected#) 1))
+                    result# (with-out-str (runBefunge ~testPath))]
+                (is (= trimExpected# result#))))))))
 
 (runPyfungeTests)
