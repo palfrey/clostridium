264e94e2b80bdb2deb865ddcbacfc9ae37b3db69
Tom Parker <palfrey@tevp.net>
2017-08-13 19:21:40 +0100

    Add row/column display

--- src/clostridium/core.cljs
+++ src/clostridium/core.cljs
@@ -1,6 +1,8 @@
 (ns clostridium.core
   (:require [reagent.core :as r]
-            [clostridium.befunge :as befunge]))
+            [clostridium.befunge :as befunge]
+            [goog.string :as gstring]
+            [goog.string.format]))
 
 (defonce app-state (r/atom {}))
 
@@ -9,13 +11,19 @@
         data (:grid b)
         pc (:pc b)]
     [:div
-     (doall (for [row (keys data)
-                  column (keys (get data row))
-                  value (get (get data row) column)
-                  :let [id (str row "-" column "-")]]
-              ^{:key (str id "sq")}
-              [:div {:class (str (if (= pc [column row]) "active " "") "square")}
-               value]))]))
+     (concat
+      [^{:key "gap"} [:div {:class "square"} (gstring/unescapeEntities "&nbsp;")]]
+      (doall (for [column (keys (get data 0))]
+               ^{:key (gstring/format "col-%d" column)} [:div {:class "square column"} column]))
+      [^{:key "split"} [:br {:style {:clear "both"}}]]
+      (doall (for [row (keys data)]
+               (cons ^{:key (gstring/format "row-%d" row)} [:div {:class "square row"} row]
+                     (doall (for [column (keys (get data row))
+                                  value (get (get data row) column)
+                                  :let [id (str row "-" column "-")]]
+                              ^{:key (str id "sq")}
+                              [:div {:class (str (if (= pc [column row]) "active " "") "square")}
+                               value]))))))]))
 
 (defn dev-setup []
   (when ^boolean js/goog.DEBUG
