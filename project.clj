755c3e748bfa83db3c4ea50dd91aea447c582444
Tom Parker <palfrey@tevp.net>
2017-08-14 23:37:34 +0100

    Upgrade Clojurescript

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "GNU Affero General Public License v3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/clojurescript "1.9.854"]
                  [reagent "0.7.0"]
                  [funcool/decimal "1.0.1"]]
   :main clostridium.core
